7ba0e28c65f6c09712d8cca5f31b259e372613c0
stephendj <stephendj181194@gmail.com>
2015-08-11 23:29:58 +0700

    Added Accound DataObjects and added db in mobile context

--- Evenue.BackEndAPI/DataObjects/Account.cs
+++ Evenue.BackEndAPI/DataObjects/Account.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Evenue.BackEndAPI.DataObjects
 {
-    public class Account
+    public class Account : EntityData
     {
+        public string Username { get; set; }
+        public byte[] Salt { get; set; }
+        public byte[] SaltedAndHashedPassword { get; set; }
     }
 }
